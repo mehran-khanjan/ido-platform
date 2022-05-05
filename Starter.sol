e515e976ec7c80f36d1a55789a4e50f029c8d158
mehran-khanjan <mehrankh1913@gmail.com>
2022-05-05 10:17:02 +0430

    Add Starter Smart Contract

---
+++ Starter.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-3.0-only
+pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
+import "../base/Sweepable.sol";
+
+contract SuperStarter is Ownable, ReentrancyGuard, Sweepable {
+    using SafeERC20 for IERC20;
+}
