46c0dcbb8232b77ad8bc57c4a91e9517792a634e
mehran-khanjan <mehrankh1913@gmail.com>
2022-05-17 08:22:55 +0430

    Add another event

--- Starter.sol
+++ Starter.sol
@@ -57,4 +57,5 @@ contract SuperStarter is Ownable, ReentrancyGuard, Sweepable {
     );
 
     event Claim(uint256 id, address indexed claimer, uint256 amount, uint256 timestamp);
+    event PoolFinished(uint256 id, uint256 timestamp);
 }
