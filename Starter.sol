b785a9f6bddf9c8e496845f433de263f1cd6ef96
mehran-khanjan <mehrankh1913@gmail.com>
2022-07-04 13:48:03 +0430

    add some events

--- Starter.sol
+++ Starter.sol
@@ -302,4 +302,21 @@ contract SuperStarter is Ownable, ReentrancyGuard, Sweepable {
         bytes32 clearingPriceOrder
     );
     event UserRegistration(address indexed user, uint64 userId);
+
+    struct AuctionData {
+        IERC20 auctioningToken;
+        IERC20 biddingToken;
+        uint256 orderCancellationEndDate;
+        uint256 auctionEndDate;
+        bytes32 initialAuctionOrder;
+        uint256 minimumBiddingAmountPerOrder;
+        uint256 interimSumBidAmount;
+        bytes32 interimOrder;
+        bytes32 clearingPriceOrder;
+        uint96 volumeClearingPriceOrder;
+        bool minFundingThresholdNotReached;
+        bool isAtomicClosureAllowed;
+        uint256 feeNumerator;
+        uint256 minFundingThreshold;
+    }
 }
