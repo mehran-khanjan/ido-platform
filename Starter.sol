850a8fe3f8af0c1365b158a55ce206e891ea4387
mehran-khanjan <mehrankh1913@gmail.com>
2022-07-23 11:25:35 +0430

    add code

--- Starter.sol
+++ Starter.sol
@@ -349,4 +349,18 @@ contract SuperStarter is Ownable, ReentrancyGuard, Sweepable {
         feeNumerator = newFeeNumerator;
     }
 
+    function initiateAuction(
+        IERC20 _auctioningToken,
+        IERC20 _biddingToken,
+        uint256 orderCancellationEndDate,
+        uint256 auctionEndDate,
+        uint96 _auctionedSellAmount,
+        uint96 _minBuyAmount,
+        uint256 minimumBiddingAmountPerOrder,
+        uint256 minFundingThreshold,
+        bool isAtomicClosureAllowed,
+        address accessManagerContract,
+        bytes memory accessManagerContractData
+    ) public returns (uint256) {}
+
 }
