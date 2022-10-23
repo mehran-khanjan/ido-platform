143b0d4acb25ee0f47bd1a244cea3a5dc25b490c
mehran-khanjan <mehrankh1913@gmail.com>
2022-10-23 10:05:36 +0330

    add more codes

--- Starter.sol
+++ Starter.sol
@@ -656,6 +656,9 @@ contract SuperStarter is Ownable, ReentrancyGuard, Sweepable {
         bytes32[] memory _prevSellOrders,
         bytes calldata allowListCallData,
         address orderSubmitter
-    ) internal returns (uint64 userId) {}
+    ) internal returns (uint64 userId) {
+        
+        address allowListManager = auctionAccessManager[auctionId];
+    }
 
 }
