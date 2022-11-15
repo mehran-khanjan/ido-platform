1a71c3115494796277cbe4ef7c00c35bb8c373a4
mehran-khanjan <mehrankh1913@gmail.com>
2022-11-15 09:13:03 +0330

    Add more codes

--- Starter.sol
+++ Starter.sol
@@ -738,6 +738,7 @@ contract SuperStarter is Ownable, ReentrancyGuard, Sweepable {
     {
         uint64 userId = getUserId(msg.sender);
         uint256 claimableAmount = 0;
+        for (uint256 i = 0; i < _sellOrders.length; i++) {}
     }
 
 }
