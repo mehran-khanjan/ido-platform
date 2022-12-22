55b98e39abf79c4d367b1485d5d9eb3590bfff1c
mehran-khanjan <mehrankh1913@gmail.com>
2022-12-22 09:04:46 +0330

    Add more codes

--- Starter.sol
+++ Starter.sol
@@ -851,6 +851,10 @@ contract SuperStarter is Ownable, ReentrancyGuard, Sweepable {
                     uint96 buyAmountOfIter,
                     uint96 sellAmountOfIter
                 ) = _sellOrders[i].decodeOrder();
+                require(
+                    userIdOfIter == userId,
+                    "Only the user can cancel his orders"
+                );
                 }
         }
     }
